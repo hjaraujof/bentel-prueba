81e3a4f117f4a2a4b92e4547ef9c02b0bbfb4cf3
Ubuntu <ubuntu@ip-172-31-40-88.us-west-2.compute.internal>
2017-08-30 06:00:46 +0000

    sloppy productin settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,4 +1,4 @@
 from .base import *
 
-SECRET_KEY = env('DJANGO_SECRET_KEY', default='$xybrfuqll#4hxrl&w#z(!r4_$0&@i_9vzf%l+k0)uzvmc1nue')
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='a$xybrfuqll#4hxrl&w#z(!r4_$0&@i_9vzf%l+k0)uzvmc1nue')
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
